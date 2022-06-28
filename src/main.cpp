795dba124073b0776b685d9230483bc9c13590b2
hamoun <hamounheidarshenas@gmail.com>
2022-06-28 19:56:17 +0200

    tuesday

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,12 @@
 
 typedef actionlib::SimpleActionServer<user_defined_msgs::BodyDetectedActionAction> Server;
 
+const int LEFT_ELBOW = 6;
+const int LEFT_WRIST = 7;
+const int RIGHT_ELBOW = 3;
+const int RIGHT_WRIST = 4;
+
+
 
 void configureWrapper(op::Wrapper& opWrapper)
 {
@@ -144,7 +150,9 @@ class ActionServerManager{
 
             // sensor_msgs::ImageConstPtr fisheye1 = ros::topic::waitForMessage<sensor_msgs::Image>("/t265/fisheye1/image_raw", ros::Duration(0.0)) ;
 
+
             sensor_msgs::Image fisheye1 = goal->fisheye1;
+            ROS_ERROR_STREAM("Goal recieved. Image captured!  " << fisheye1.header.seq);
 
         
             cv_bridge::CvImagePtr cv_ptr;
@@ -158,13 +166,42 @@ class ActionServerManager{
               return;
             }
 
-            // cv::imshow("win", cv_ptr->image);
-            // cv::waitKey(3);
-
             // const cv::Mat cvImageToProcess = cv::imread("frame0460.jpg");
             const op::Matrix imageToProcess = OP_CV2OPCONSTMAT(cv_ptr->image);
             auto datumProcessed = opw_.emplaceAndPop(imageToProcess);
             
+            auto keypoints = datumProcessed->at(0)->poseKeypoints;
+            // ROS_INFO_STREAM(keypoints.getSize(1));
+            int num_peeple = keypoints.getSize(0);
+            int num_body_part = keypoints.getSize(1);
+            int rwx = 0, rwy = 0;
+
+
+            int max = 0;
+            int index = 0;
+                            ROS_WARN_STREAM("OKKKK2222");
+
+            for(int i = 0; i < num_peeple; i++){
+
+                const int left_wrist_baseIndex = keypoints.getSize(2)*(i*num_body_part + LEFT_WRIST);
+                ROS_WARN_STREAM("OKKKK");
+                if (max < keypoints[left_wrist_baseIndex + 2]){
+                    max = keypoints[left_wrist_baseIndex + 2];
+                    index = i;
+                }
+            }
+
+
+            for(int i = 0; i < num_peeple; i++){
+                i = index;    
+                const auto left_elbow_baseIndex = keypoints.getSize(2)*(index*num_body_part + LEFT_ELBOW);
+                const auto left_wrist_baseIndex = keypoints.getSize(2)*(index*num_body_part + LEFT_WRIST);
+                const auto right_elbow_baseIndex = keypoints.getSize(2)*(index*num_body_part + RIGHT_ELBOW);
+                const auto right_wrist_baseIndex = keypoints.getSize(2)*(index*num_body_part + RIGHT_WRIST);
+                rwx = keypoints[left_wrist_baseIndex];
+                rwy = keypoints[left_wrist_baseIndex + 1];
+            }
+
             user_defined_msgs::BodyDetectedActionResult result;
             user_defined_msgs::EgocentricBodySegmentDetected msg;
             
@@ -173,8 +210,12 @@ class ActionServerManager{
             result.detected_segments.push_back(msg); 
 
             server_.setSucceeded(result);
+
+            cv::circle(cv_ptr->image, cv::Point(rwx, rwy), 2, cv::Scalar(0, 255, 0), 2);
+            cv::imshow("win", cv_ptr->image);
+            cv::waitKey(3);
             
-            ROS_ERROR_STREAM("Goal recieved. Image captured!  " << fisheye1.header.seq);
+            ROS_ERROR_STREAM("Goal Processed.################" << fisheye1.header.seq);
 
 
         }
@@ -194,7 +235,7 @@ int main(int argc, char **argv){
 
     op::Wrapper opWrapper{op::ThreadManagerMode::Asynchronous};
     configureWrapper(opWrapper);
-    opWrapper.start();
+    // opWrapper.start();
     
     ActionServerManager action_server(opWrapper);
 
